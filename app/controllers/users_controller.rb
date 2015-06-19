4a72d01b8742041ecac143533963e732ff471462
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-19 13:13:42 -0500

    added index new create destory and user_params to user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = User.all
+    render json: @user
+  end
+
+  def new
+    @user = User.new
+    render json: @user
+  end
+
+  def create
+    @user = User.create(user_params)
+    render json: @user
+  end
+
+  def destory
+    @user = User.find_by_email(params[:email])
+    @user.destory
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :twitter, :linked_in, :network_circle)
+  end
 end
