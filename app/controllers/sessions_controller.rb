f731eac8dd7f9abe33dc010935ec97c4a135245f
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-18 20:55:06 -0500

    added session controller and added session methods to app controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+
+    if user != nil && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Welcome #{user.name}"
+    else
+      flash[:alert] = "Hi, looks like your email or password does not match"
+      render :new
+    end
+  end
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+end
