2a02d736cbb572b6776436d680a1564b6911ff41
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-18 20:31:10 -0500

    created foods model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :foods
-  has_many :matches
-  has_many :messages
+  has_many :foods, dependent: :destroy
+  has_many :matches, dependent: :destroy
+  has_many :messages, dependent: :destroy
   validates :name, presence: { message: "Hi, looks like you forgot to add your name." }
   validates :email, presence: { message: "Hi, looks like you forgot to add an email" }
   validates :network_cirle, presence: { message: "Hi, looks you forgot to select a cirle" }
