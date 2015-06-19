16ecf0d07a81655b26169d73b9d211f5d93468b4
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-18 19:17:42 -0500

    created match model and updated user model to validate name, email and network circle

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   has_many :foods
   has_many :matches
   has_many :messages
+  validates :name, presence: { message: "Hi, looks like you forgot to add your name." }
+  validates :email, presence: { message: "Hi, looks like you forgot to add an email" }
+  validates :network_cirle, presence: { message: "Hi, looks you forgot to select a cirle" }
 end
