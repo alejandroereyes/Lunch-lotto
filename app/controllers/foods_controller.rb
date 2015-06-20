40d5c197ba8fedd0dee18d2c64532b6dffc6a242
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-19 21:50:56 -0500

    user can now create a food personality in foods table

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -11,21 +11,21 @@ class FoodsController < ApplicationController
 
   def create
     @foods = Food.new
-    if authenticate_user! && !(Food.exists?(user_id: session[:user_id]))
-      @foods[:user_id] = session[:user_id]
-      @foods[:easy_breezy] = params(:easy_breezy)
-      @foods[:health_nut] = params(:health_nut)
-      @foods[:wild_child] = params(:wild_child)
-      @foods[:lux_lunch] = params(:lux_lunch)
-      @foods[:casual_sit_down] = params(:casual_sit_down)
+    # if authenticate_user! && !(Food.exists?(user_id: session[:user_id]))
+      @foods[:user_id] = params[:user_id]
+      @foods[:easy_breezy] = params[:easy_breezy] if params.has_key?(:easy_breezy)
+      @foods[:health_nut] = params[:health_nut] if params.has_key?(:health_nut)
+      @foods[:wild_child] = params[:wild_child] if params.has_key?(:wild_child)
+      @foods[:lux_lunch] = params[:lux_lunch] if params.has_key?(:lux_lunch)
+      @foods[:casual_sit_down] = params[:casual_sit_down] if params.has_key?(:casual_sit_down)
       if @foods.save
         render json: @foods
       else
         render json: { error: "problem saving foods, check params", params: params }, status: 500
       end
-    else
-      render json: { error: "User not found" }, status: 404
-    end
+    # else
+    #   render json: { error: "User not found" }, status: 404
+    # end
   end
 
   def update
